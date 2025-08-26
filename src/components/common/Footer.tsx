716852753c34db71241b1fd10a992cd56b21b32f
Kenta Urakami <ken.atnek@gmail.com>
2025-08-26 19:59:20 +0900

    🧑‍💻 開発準備

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,14 @@
+/* =======================================
+ *リタワーク FOOTER
+ * URL: src/components/common/Footer.tsx
+ * Created: 2025-07-28
+ * Last updated: 2025-07-28
+ * ======================================= */
+import styles from '@/styles/components/common/Footer.module.scss';
+import Link from 'next/link';
+import ExternalLink from '@/components/common/ExternalLink';
+const Footer = () => {
+  return <footer className={styles.containerFooter}></footer>;
+};
+
+export default Footer;
