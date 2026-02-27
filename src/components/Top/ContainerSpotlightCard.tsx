692ae66c1ecfd78ad03a651cd657992a1ab976b8
KEN-MBA <165755076+KEN-MBA@users.noreply.github.com>
2026-02-27 18:02:19 +0900

    TOP 注目の求人スライド調整

--- src/components/Top/ContainerSpotlightCard.tsx
+++ src/components/Top/ContainerSpotlightCard.tsx
@@ -37,7 +37,7 @@ import {
  * -------------------------------------- */
 const splideOptions: Options = {
   type: 'loop',
-  perPage: 4,
+  perPage: 3,
   perMove: 1,
   gap: '18px',
   autoplay: false,
@@ -46,6 +46,7 @@ const splideOptions: Options = {
   speed: 400,
   arrows: true,
   pagination: false,
+  padding: { left: '12%', right: '12%' },
   drag: true,
   // autoHeight: true,
   breakpoints: {
